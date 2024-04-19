3962abf6afb932c13ac50b61ea09142e7e03cdb6
alaneta66 <166676231+alaneta66@users.noreply.github.com>
2024-04-19 12:45:42 +0800

    feat: first commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+Steam Automation Tool
+
+TIPS:
+
+1. find the Steam register api url
+2. find the Steam reCaptcha websiteKey and reCaptcha version and the config
+3. get the [NextCaptcha](https://nextcaptcha.com/) clientKey and set as environment variables NEXTCAPTCHA_KEY
+4. solve the reCaptcha with NextCaptcha then replace the reCAPTCHA token
+5. send the request
+6. register success
+
+7. DISCLAIMERS:
+
+This tool is not affiliated, associated, or partnered with Steam in any way. We are not authorized, endorsed, or sponsored by Steam. All Steam trademarks remain the property of Fenix International Limited.
+This is a theoritical program only and is for educational purposes. If you choose to use it then it may or may not work. You solely accept full responsability and indemnify the creator, hostors, contributors and all other involved persons from any any all responsability.
---
+++ src/register.ts
@@ -0,0 +1,108 @@
+import puppeteer from "puppeteer"
+import NextCaptcha from "nextcaptcha-ts";
+import _ from 'lodash';
+
+const url = 'https://store.steampowered.com/join';
+
+function main() {
+  puppeteer.launch({
+    headless: true,
+    args: ['--proxy-server=http://127.0.0.1:10809']
+  }).then(async browser => {
+    const clientKey = process.env.NEXTCAPTCHA_KEY || '';
+    const client = new NextCaptcha(clientKey);
+    let clientConfig: any;
+    if (process.argv.length < 3) throw Error('Usage: register.ts <email>');
+    const [email, password] = process.argv.slice(2);
+    const page = await browser.newPage();
+
+    page.on('response', async response => {
+        if ( response.url().includes('/join/ajaxverifyemail')) {
+          console.log(await response.json())
+          const data = await response.json();
+          if (data.success === 1) {
+            console.log('register success, please check your email')
+          } else {
+            console.log('register fail: ', data)
+          }
+          process.exit(0);
+        }
+    })
+
+    await page.goto('https://store.steampowered.com/join', {
+      waitUntil: 'networkidle0',
+    });
+
+    clientConfig = (await page.evaluate(() => {
+
+      function findCaptchaInfo(widget: any) {
+          let result: any = {};
+          function recurse(obj: any) {
+            if (Object.entries(result).length === 3) return;
+            for (let key in obj) {
+              if (key === 'sitekey' || key === 's' || key === 'action') {
+                result[key] = obj[key];
+              } else if ((Object.prototype.toString.call(obj[key]) === '[object Object]')) {
+                recurse(obj[key]);
+              }
+            }
+        }
+        recurse(widget);
+        return result;
+      }
+
+      return new Promise<any[]>(resolve => {
+        let times = 30;
+        const id = setInterval(() => {
+          if (times < 0) resolve([]);
+          times--;
+          // @ts-ignore
+          if (window.___grecaptcha_cfg && window.___grecaptcha_cfg.clients) {
+            const res: any = [];
+            // @ts-ignore
+            Object.entries(window.___grecaptcha_cfg.clients).forEach((widget: [string, any])=> {
+              res.push(findCaptchaInfo(widget[1]));
+            })
+            clearInterval(id);
+            resolve(res);
+          }
+        }, 1000)
+      })
+
+    }))?.[0];
+    if (_.isEmpty(clientConfig)) {
+      console.error('get captcha config error')
+
+    }
+    await page.setViewport({width: 1080, height: 1024});
+    const emailInput = await page.waitForSelector('#email');
+    await emailInput?.focus();
+    await emailInput?.type(email);
+    const confirmEmailInput = await page.waitForSelector('#reenter_email');
+    await confirmEmailInput?.focus();
+    await confirmEmailInput?.type(email);
+    const checkbox = await page.waitForSelector('#i_agree_check');
+    await checkbox?.click()
+    const token = await client.recaptchaV2Enterprise({
+      websiteKey: clientConfig?.sitekey,
+      websiteURL: url,
+      // pageAction: clientConfig?.action,
+      // isInvisible: false,
+      enterprisePayload: {
+        s: clientConfig?.s as string
+      } as any
+    });
+    await page.evaluate((gRecaptchaResponse) => {
+      document.querySelector<HTMLTextAreaElement>('[name="g-recaptcha-response"]')!.value = gRecaptchaResponse;
+      document.querySelector<HTMLTextAreaElement>('[name="g-recaptcha-response"]')!.innerText = gRecaptchaResponse;
+    }, token.solution.gRecaptchaResponse);
+    const registerBtn = await page.waitForSelector('#createAccountButton');
+    await registerBtn?.click()
+    await browser.close()
+  }).catch(err => {
+    console.log(err);
+    process.exit(0)
+  })
+}
+
+main()
